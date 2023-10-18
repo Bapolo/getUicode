c13e5c4550082a8b92a597fdb8542f487848cd7a
Voldemort <Bapolo>
2023-10-18 14:38:35 +0100

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", () =>
+{
+
+    function getUnicodePoint(charactere)
+    {
+        if (charactere.length === 1)
+        {
+            const unicodePoint = charactere.charCodeAt(0).toString(16)
+            return `U+${unicodePoint}`
+        }
+    }
+
+    const input = document.querySelector("input")
+    const output = document.querySelector("output")
+    const btnCopy = document.querySelector("#saida")
+    const feedback = document.querySelector("#feedback")
+
+    input.addEventListener("input", () =>
+    {
+        if (input.value.length !== 0)
+        {
+            output.innerHTML =  getUnicodePoint(input.value)
+            return false
+        }
+        
+        output.innerHTML =  ""
+        return false
+    })
+
+
+    btnCopy.addEventListener("click", () =>
+    {
+        if (output.value.length !== 0)
+        {
+            const textarea = document.createElement("textarea")
+            
+            let textoFormatado = output.textContent.slice(2)
+            textoFormatado = "\\" + textoFormatado
+            textarea.value = textoFormatado
+    
+            document.body.appendChild(textarea)
+    
+            textarea.select()
+    
+            document.execCommand("copy")
+    
+            document.body.removeChild(textarea)
+
+            feedback.style.display = "inline-block"
+            feedback.style.animation = "fadeIn 0.2s linear"
+
+            setTimeout(() => 
+            {
+                feedback.style.display = "none"
+            }, 2000)
+
+            output.innerHTML = ""
+            input.value = ""
+            
+            
+
+            return false
+        }
+       
+    })
+})
